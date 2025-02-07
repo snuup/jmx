77a4861a943e2760e10f04db54cc6d461c6ed4a8
snuup <hop@home.com>
2025-02-07 05:45:05 +0100

    tests

--- vite.config.test.ts
+++ vite.config.test.ts
@@ -1,36 +1,26 @@
 import { jmxplugin } from "./jmx/vite-plugin-jmx" // "./jmx/ plugin/plugin/vite-plugin-jmx"
+import * as shared from "./vite.config.shared"
 
 export default {
-    base: "/",
-    //root: "./app",
-    esbuild: {
-        ignoreAnnotations: true,
-        target: 'esnext',
-    },
-    plugins: [
-        jmxplugin()
-    ],
+    ...shared,
+    // base: "/",
     build: {
-        outDir:"dist",
-        emptyOutDir: true,
-        target: 'esnext', // !!
-        minify: false,
         rollupOptions: {
-                output: {
-                    entryFileNames: `app.js`,
-                    assetFileNames: `[name].[ext]`
-                },
+                // output: {
+                //     entryFileNames: `app.js`,
+                //     assetFileNames: `[name].[ext]`
+                // },
                 input: {
                     tests: 'tests/demos.test.tsx', // Replace with your test entry file
                 },
                 outDir: 'dist-tests', // Separate output for tests
         },
-        css: {
-            devSourcemap: true,
-        },
-        modulePreload: {
-            polyfill: false
-        }
+        // css: {
+        //     devSourcemap: true,
+        // },
+        // modulePreload: {
+        //     polyfill: false
+        // }
     },
     test: {
         environment: 'happy-dom', // Set Happy DOM as the test environment
