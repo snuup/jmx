bfaa890e990133fe593478dcc21780ae40edb932
snuup <61933552+snuup@users.noreply.github.com>
2025-02-19 17:31:03 +0100

    .

--- packages/zap/dist/app.js
+++
@@ -1,5 +0,0 @@
-let x = {
-  cn: [1, 2, 3]
-};
-console.log(x);
-//# sourceMappingURL=app.js.map
--- packages/zap/lab.tsx
+++
@@ -1,6 +0,0 @@
-import { jsx, jsxf } from "../jmx"
-
-let x = <>{1}{2}{3}</>
-// let x = jsx(jsxf, null, 1, 2, 3);
-
-console.log(x);
