3857b9038f8c7fc417ba2994c39b5aa2746cb853
snuup <hop@home.com>
2025-06-14 19:08:26 +0200

    029 globalthis

--- packages/jmx/dist/index.js
+++ packages/jmx/dist/index.js
@@ -20,8 +20,8 @@ const loggedmethodsex = (o, logger) => new Proxy(o, {
 const loggedmethods = (o) => loggedmethodsex(o, (name, args, result) => console.log("%c" + name, "background:#585059;color:white;padding:2px;font-weight:bold", args));
 const loggedmethodscolored = (bgcolor, o) => loggedmethodsex(o, (name, args, result) => console.log("%c" + name, `background:${bgcolor};color:white;padding:2px;font-weight:bold`, args));
 
-window.jmx = {
-    create: tagName => document.createElement(tagName)
+globalThis.jmx = {
+    create: (tagName) => document.createElement(tagName)
 };
 let evaluate = (expr) => expr instanceof Function ? expr() : expr;
 let removeexcesschildren = (n, i) => { let c; while ((c = n.childNodes[i])) {
